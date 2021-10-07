0bcf9f9317934d37754ba8b0dc6bc608e5de8951
Alex Selby <alex.selby@hotmail.com>
2021-10-07 15:10:48 +0100

    implemented 2 paddles that can move with key presses

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from turtle import Screen
+from paddle import Paddle
+import time
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
 
+screen.listen()
+screen.onkey(key="Up", fun=r_paddle.move_up)
+screen.onkey(key="Down", fun=r_paddle.move_down)
+screen.onkey(key="w", fun=l_paddle.move_up)
+screen.onkey(key="s", fun=l_paddle.move_down)
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
 
