a3050c9a037eb48c6207c8c209f109c964bb75b0
Alex Selby <alex.selby@hotmail.com>
2021-10-07 15:24:11 +0100

    adding moving ball to screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 import time
 
 screen = Screen()
@@ -11,16 +12,20 @@ screen.tracer(0)
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
 
+ball = Ball()
+
+
 screen.listen()
 screen.onkey(key="Up", fun=r_paddle.move_up)
 screen.onkey(key="Down", fun=r_paddle.move_down)
 screen.onkey(key="w", fun=l_paddle.move_up)
 screen.onkey(key="s", fun=l_paddle.move_down)
 
-
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 screen.exitonclick()
 
